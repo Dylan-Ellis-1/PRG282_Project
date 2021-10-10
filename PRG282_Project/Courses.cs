ee0c09a2857ad67e6cc6480f0b514840a7d5aa47
MiguelSwanepoel <576798@student.belgiumcampus.ac.za>
2021-10-10 23:11:02 +0200

    Added few stuff just to see if I can do this right

--- PRG282_Project/Courses.cs
+++ PRG282_Project/Courses.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PRG282_Project
 {
-    public partial class Form1 : Form
+    public partial class Courses : Form
     {
-        public Form1()
+        public Courses()
         {
             InitializeComponent();
         }
