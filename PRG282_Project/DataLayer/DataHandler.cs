90db94b96e78c37c581fb55e438cea72202f2464
Dylan <dylan00ellis@gmail.com>
2021-10-11 21:41:20 +0200

    changed DataLayer to make use of stored procedures

--- PRG282_Project/DataLayer/DataHandler.cs
+++ PRG282_Project/DataLayer/DataHandler.cs
@@ -15,15 +15,15 @@ namespace PRG282_Project.DataLayer
         {
         }
                                         
-        static string con = "Server=.; Initial Catalog= [Project]; Integrated Security=SSPI";
-
-        SqlConnection cn = new SqlConnection(con);
+        string con = "Server=.; Initial Catalog= [Project]; Integrated Security=SSPI";
         
         public DataTable getStudent()
         {
-            string query = "SELECT * FROM Students";
+            SqlConnection cn = new SqlConnection(con);
 
-            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter("spGetStudents", con);
+
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
 
             DataTable studentData = new DataTable();
 
@@ -34,142 +34,11 @@ namespace PRG282_Project.DataLayer
 
         public DataTable getCourse()
         {
-            string query = "SELECT * FROM Courses";
+            SqlConnection cn = new SqlConnection(con);
 
-            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter("spGetCourses", con);
 
-            DataTable courseData = new DataTable();
-
-            adapter.Fill(courseData);
-
-            return courseData;
-        }
-
-        public string updateStudent(Student student)
-        {
-            try
-            {
-                cn.Open();
-            }
-            catch (Exception eA)
-            {
-                MessageBox.Show("Error while connecting to the database: " + eA);
-            }
-
-            try
-            {
-                string query = "UPDATE Students SET NameSurname = '" + student.NameSurname + "', ImgPath = '" + student.ImagePath + "', StudentDOB = '" + student.Dob + "', Gender = '" + student.Gender + "', Phone = '" + student.Phone + "', Address_ = '" + student.Address + "' WHERE StudentNumber = '" + student.StudentNumber + "'";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "Student details have been updated";
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nStudent details could not be updated.";
-            }
-        }
-
-        public string updateCourse(Course course)
-        {
-            try
-            {
-                cn.Open();
-            }
-            catch (Exception eA)
-            {
-                MessageBox.Show("Error while connecting to the database: " + eA);
-            }
-
-            try
-            {
-                string query = "UPDATE Courses SET ModName = '" + course.Name + "', ModDesc = '" + course.Description + "', Link = '" + course.ResourcesLink + "' WHERE ModCode = '" + course.CourseCode + "'";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "Module details have been updated.";
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nModule details could not be updated";
-            }
-        }
-
-        public string deleteStudent(string id)
-        {
-            try
-            {
-                cn.Open();
-            }
-            catch (Exception eA)
-            {
-                MessageBox.Show("Error while connecting to the database: " + eA);
-            }
-
-            try
-            {
-                string query = "DELETE FROM Students WHERE StudentNumber = '" + id + "'";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "Deleted details of Student with Student number: " + id;
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nCould not delete the Student.";
-            }
-
-            
-        }
-
-        public string deleteCourse(string id)
-        {
-            try
-            {
-                cn.Open();
-            }
-            catch (Exception eA)
-            {
-                MessageBox.Show("Error while connecting to the database: " + eA);
-            }
-
-            try
-            {
-                string query = "DELETE FROM Courses WHERE ModCode = '" + id + "'";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "Deleted details of the Module with Module code: " + id;
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nCould not delete the Module details";
-            }
-
-            
-        }
-
-        public DataTable searchStudent(string id)
-        {
-            string query = "SELECT * FROM Students WHERE StudentNumber = '" + id + "'";
-
-            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
 
             DataTable studentData = new DataTable();
 
@@ -178,88 +47,314 @@ namespace PRG282_Project.DataLayer
             return studentData;
         }
 
+        public string updateStudent(Student student, List<string> courses)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spUpdateStudents", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                    cmd.Parameters.AddWithValue("@NameSurname", student.NameSurname);
+                    cmd.Parameters.AddWithValue("@Gender", student.Gender);
+                    cmd.Parameters.AddWithValue("@DOB", student.Dob);
+                    cmd.Parameters.AddWithValue("@Phone", student.Phone);
+                    cmd.Parameters.AddWithValue("@Address", student.Address);
+                    cmd.Parameters.AddWithValue("@ImgPath", student.ImagePath);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteJoingCourses", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@StudnetNumber", student.StudentNumber);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                foreach (string course in courses)
+                {
+                    using (SqlConnection cn = new SqlConnection(con))
+                    {
+                        SqlCommand cmd = new SqlCommand("spAddStudentCourses", cn);
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                        cmd.Parameters.AddWithValue("@ModCode", course);
+
+                        cn.Open();
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                    }
+                }
+
+                return "Student data updated successfully.";
+            }
+            catch (Exception eA)
+            {
+                return "The following error was encountered while trying to update Student data:\n\n" + eA.Message;
+            }
+        }
+
+        public string updateCourse(Course course)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spUpdateCourses", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ModCode", course.CourseCode);
+                    cmd.Parameters.AddWithValue("@ModName", course.Name);
+                    cmd.Parameters.AddWithValue("@ModDesc", course.Description);
+                    cmd.Parameters.AddWithValue("@Link", course.ResourcesLink);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                return "Course data updated successfully.";
+            }
+            catch (Exception eA)
+            {
+                return "The following error was encountered while trying to update Course data:\n\n" + eA.Message;
+            }
+        }
+
+        public string deleteStudent(string id)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteStudents", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@StudnetNumber", id);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                return "Student data deleted successfully.";
+            }
+            catch (Exception eA)
+            {
+                return "The following error was encountered while trying to delete Student data:\n\n" + eA.Message;
+            }
+        }
+
+        public string deleteCourse(string id)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteCourses", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@CourseCode", id);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                return "Course data deleted successfully.";
+            }
+            catch (Exception eA)
+            {
+                return "The following error was encountered while trying to delete Course data:\n\n" + eA.Message;
+            }
+        }
+
+        public string deleteCourseForStudent(string courseID, string studentID)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spDeleteCoursesStudent", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@CourseCode", courseID);
+                    cmd.Parameters.AddWithValue("@StudentNumber", studentID);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                return "Course data for Student deleted successfully.";
+            }
+            catch (Exception eA)
+            {
+                return "The following error was encountered while trying to delete Course data of the Student:\n\n" + eA.Message;
+            }
+        }
+
+        public DataTable searchStudent(string id)
+        {
+            using (SqlConnection cn = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("spSearchStudents", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@StudnetNumber", id);
+
+                cn.Open();
+
+                DataTable data = new DataTable();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    data.Load(dr);
+
+                    return data;
+                }
+            }
+        }
+
         public DataTable searchCourse(string id)
         {
-            string query = "SELECT * FROM Courses WHERE ModCode = '" + id + "'";
+            using (SqlConnection cn = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("spSearchCourses", cn);
 
-            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                cmd.CommandType = CommandType.StoredProcedure;
 
-            DataTable courdeData = new DataTable();
+                cmd.Parameters.AddWithValue("@CourseCode", id);
 
-            adapter.Fill(courdeData);
+                cn.Open();
 
-            return courdeData;
+                DataTable data = new DataTable();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    data.Load(dr);
+
+                    return data;
+                }
+            }
         }
 
         public DataTable populateCourse(string id)
         {
-            string query = "SELECT * FROM StudentCourses WHERE StudentNumber = '" + id + "'";
+            using (SqlConnection cn = new SqlConnection(con))
+            {
+                SqlCommand cmd = new SqlCommand("spPopulateCourses", cn);
 
-            SqlDataAdapter adapter = new SqlDataAdapter(query, con);
+                cmd.CommandType = CommandType.StoredProcedure;
 
-            DataTable studentCourseData = new DataTable();
+                cmd.Parameters.AddWithValue("@StudnetID", id);
 
-            adapter.Fill(studentCourseData);
+                cn.Open();
 
-            return studentCourseData;
+                DataTable data = new DataTable();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    data.Load(dr);
+
+                    return data;
+                }
+            }
         }
 
-        public string addStudent(Student student)
+        public string addStudent(Student student, List<string> courses)
         {
             try
             {
-                cn.Open();
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spAddStudents", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                    cmd.Parameters.AddWithValue("@NameSurname", student.NameSurname);
+                    cmd.Parameters.AddWithValue("@Gender", student.Gender);
+                    cmd.Parameters.AddWithValue("@DOB", student.Dob);
+                    cmd.Parameters.AddWithValue("@Phone", student.Phone);
+                    cmd.Parameters.AddWithValue("@Address", student.Address);
+                    cmd.Parameters.AddWithValue("@ImgPath", student.ImagePath);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                foreach (string course in courses)
+                {
+                    using (SqlConnection cn = new SqlConnection(con))
+                    {
+                        SqlCommand cmd = new SqlCommand("spAddStudentCourses", cn);
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@StudentNumber", student.StudentNumber);
+                        cmd.Parameters.AddWithValue("@ModCode", course);
+
+                        cn.Open();
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                    }
+                }
+
+                return "Student data added successfully.";
             }
             catch (Exception eA)
             {
-                MessageBox.Show("Error while connecting to the database: " + eA);
+                return "The following error was encountered while trying to add Student data:\n\n" + eA.Message;
             }
-
-            try
-            {
-                string query = "INSERT INTO Students (StudentNumber, NameSurname, ImgPath, StudentDOB, Gender, Phone, Address_) VALUES ('" + student.StudentNumber + "', '" + student.NameSurname + "', '" + student.ImagePath + "', '" + student.Dob + "', '" + student.Gender + "', '" + student.Phone + "', '" + student.Address + "')";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "A new Student has been added.";
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nThe new Student could not be added.";
-            } 
         }
 
         public string addCourse(Course course)
         {
             try
             {
-                cn.Open();
+                using (SqlConnection cn = new SqlConnection(con))
+                {
+                    SqlCommand cmd = new SqlCommand("spAddCourses", cn);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@ModCode", course.CourseCode);
+                    cmd.Parameters.AddWithValue("@ModName", course.Name);
+                    cmd.Parameters.AddWithValue("@ModDesc", course.Description);
+                    cmd.Parameters.AddWithValue("@Link", course.ResourcesLink);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+
+                return "Course data added successfully.";
             }
             catch (Exception eA)
             {
-                MessageBox.Show("Error while connecting to the database: " + eA);
+                return "The following error was encountered while trying to add Course data:\n\n" + eA.Message;
             }
-
-            try
-            {
-                string query = "INSERT INTO Courses (ModCode, ModName, ModDesc, Link) VALUES ('" + course.CourseCode + "', '" + course.Name + "', '" + course.Description + "', '" + course.ResourcesLink + "')";
-
-                SqlCommand cmd = new SqlCommand(query, cn);
-
-                cmd.ExecuteNonQuery();
-
-                cn.Close();
-
-                return "A new Module has been added.";
-            }
-            catch (Exception eE)
-            {
-                return eE.Message + "\nThe new Module could not be added.";
-            } 
         }
     }
 }
